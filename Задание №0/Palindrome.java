06736668d304459509836f4d006d53edec145e02
Danil <qiol@inbox.ru>
2018-02-24 17:42:01 +0300

    Folders names are changed

--- Задание №0/Palindrome.java
+++ Задание №0/Palindrome.java
@@ -1,21 +1,35 @@
 public class Palindrome
 {
     public static String reverseString(String s){
-
         String temp = "";
 
         for (int i = s.length()-1; i >= 0; i--){
-
             temp += s.charAt(i);
         }
 
         return temp;
     }
 
+    static boolean isPalindrome(String str){
+
+        String revStr = reverseString(str);
+        if (str.equals(revStr))
+            return true;
+        else
+            return false;
+    }
 
     public static void main(String[] args){
-        String s = "pizzeria";
-        String revS = reverseString(s);
-        System.out.println(revS);
+
+        String str = "";
+        for (int i = 0; i < args.length; i++){
+            str = args[i];
+        }
+
+        if (isPalindrome(str))
+            System.out.println(str + " - palindrome");
+        else
+            System.out.println(str + " - not a palindrome");
+
     }
 }
